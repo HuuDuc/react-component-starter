f1fa34d59ef4f02225a0fc29f07d1ccd7d925347
Stéphane Nguyen <s.nguyen@outlook.com>
2023-12-25 21:56:09 -0800

    update dummy test

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
 
-import 'jest-canvas-mock';
-
 import { MyCounter } from '../src';
 
 describe('Common render', () => {
-  it('renders without crashing', () => {
+  test('renders without crashing', () => {
     render(<MyCounter />);
   });
 });
