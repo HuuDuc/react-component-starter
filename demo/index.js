5c134e5f76125e84556df8bc7f9f2ff783c19f7f
Stéphane Nguyen <s.nguyen@outlook.com>
2023-12-26 18:15:32 -0800

    move demo folder

--- demo/index.js
+++ demo/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { MyCounter } from '../index';
+import { MyCounter } from '../src/index';
 const root = createRoot(document.getElementById('demo'));
 root.render(<MyCounter />);
