b42afd42fa5aec6120f310ae3cc77e10bd484182
Stéphane Nguyen <s.nguyen@outlook.com>
2024-01-02 11:51:26 -0800

    remove styled component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import styles from './styles.module.scss';
 
 type Props = {
   value?: number;
@@ -16,27 +16,15 @@ const MyCounter = ({ value = 0 }: Props) => {
     setCounter((prev) => prev + 1);
   };
 
-  const Button = styled.button`
-    border: 1px solid #555;
-    font-weight: bold;
-    font-size: 1.5em;
-    margin: 10px;
-    padding: 4px 10px;
-  `;
-
-  const MinusButton = styled(Button)`
-    background: red;
-  `;
-
-  const PlusButton = styled(Button)`
-    background: green;
-  `;
-
   return (
     <div>
       <h1>Counter: {counter}</h1>
-      <MinusButton onClick={onMinus}>-</MinusButton>
-      <PlusButton onClick={onPlus}>+</PlusButton>
+      <button className={`${styles.btn} ${styles.red}`} onClick={onMinus}>
+        -
+      </button>
+      <button className={`${styles.btn} ${styles.green}`} onClick={onPlus}>
+        +
+      </button>
     </div>
   );
 };
